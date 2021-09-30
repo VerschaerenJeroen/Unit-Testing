214756c10267f26d7741253db32371e6c8a5a2e0
VerschaerenJeroen <verschaerenjeroen@gmail.com>
2021-09-30 14:09:31 +0200

    oef1

--- src/main/java/Exercise01/ArrayOfInts.java
+++ src/main/java/Exercise01/ArrayOfInts.java
@@ -29,7 +29,21 @@ public class ArrayOfInts {
     }
 
     public int sumOfXLargest(int[] arrayOfInts, int x) {
-        return 0;
+        if (arrayOfInts == null || arrayOfInts.length == 0) return 0;
+        int[] copy = Arrays.copyOf(arrayOfInts, arrayOfInts.length);
+        Arrays.sort(copy);
+        reverse(copy);
+        return sumOfInts(Arrays.copyOf(copy, x));
+    }
+
+    private static void reverse(int[] input) {
+        int last = input.length - 1;
+        int middle = input.length / 2;
+        for (int i = 0; i <= middle; i++) {
+            int temp = input[i];
+            input[i] = input[last - i];
+            input[last - i] = temp;
+        }
     }
 
     public static void main(String[] args) {
